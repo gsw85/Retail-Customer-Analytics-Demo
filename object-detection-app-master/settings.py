40632f54ce36af54cefb22ceff116b42f1aca6f7
nelsonlooskymind <86047295+nelsonlooskymind@users.noreply.github.com>
2021-07-19 11:33:11 +0800

    Add files via upload

---
+++ object-detection-app-master/settings.py
@@ -0,0 +1,4 @@
+DEFAULT_CONFIDENCE_THRESHOLD = 0.5
+DEMO_IMAGE = "images/demo.jpg"
+MODEL = "E:\object-detection-app-master\model\MobileNetSSD_deploy.caffemodel"
+PROTOTXT = "E:\object-detection-app-master\model\MobileNetSSD_deploy.prototxt.txt"
