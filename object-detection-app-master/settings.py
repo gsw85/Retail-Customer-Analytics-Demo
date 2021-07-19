d9ce1bb2bec29a538fb8ddcff38502fad167e130
nelsonlooskymind <86047295+nelsonlooskymind@users.noreply.github.com>
2021-07-19 11:34:03 +0800

    Delete object-detection-app-master directory

--- object-detection-app-master/settings.py
+++
@@ -1,4 +0,0 @@
-DEFAULT_CONFIDENCE_THRESHOLD = 0.5
-DEMO_IMAGE = "images/demo.jpg"
-MODEL = "E:\object-detection-app-master\model\MobileNetSSD_deploy.caffemodel"
-PROTOTXT = "E:\object-detection-app-master\model\MobileNetSSD_deploy.prototxt.txt"
